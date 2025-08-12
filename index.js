ce3a692f6c978f050ba3466b24fa972c729e7c5f
Ainish Vadhwani <ainishvadhwani24@gmail.com>
2025-08-12 19:51:45 +0530

    Added a route for interview challenge

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ import commentRoutes from './src/features/comments/comment.routes.js'
 import likeRoutes from './src/features/likes/like.routes.js'
 import friendshipRoutes from './src/features/friendship/friendship.routes.js'
 import otpRoutes from './src/features/otp/otp.routes.js'
+import { userModel } from './src/features/users/user.schema.js';
+import { postModel } from './src/features/posts/post.schema.js';
 
 const app = express()
 dotenv.config()
@@ -26,31 +28,80 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(loggerMiddleware)
 app.use(express.static('public'))
 
-app.use('/api/users',userRoutes)
-app.use('/api/posts',postRoutes)
-app.use('/api/comments',commentRoutes)
-app.use('/api/likes',likeRoutes)
-app.use('/api/friends',friendshipRoutes)
-app.use('/api/otp',otpRoutes)
-app.use('/api/docs',swaggerUi.serve, swaggerUi.setup(swaggerJSON))
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/comments', commentRoutes)
+app.use('/api/likes', likeRoutes)
+app.use('/api/friends', friendshipRoutes)
+app.use('/api/otp', otpRoutes)
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSON))
 
-app.get('/',verifyToken,(req,res)=>{
+app.get('/', verifyToken, (req, res) => {
     res.send("Server is up!")
+})
+//Interview route
+app.post('/getUserDetails', async (req, res) => {
+    try {
+        const { username } = req.body
+        const foundUser = await userModel.aggregate([
+            { $match: { email: username } },
+        ])
+        const reqDetails = await postModel.aggregate([
+            { $match: { user: foundUser[0]._id } },  //aggregation always returns array
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "targetId",
+                    as: "likesData"
+                }
+            },
+            { $addFields: { likes: "$likesData.userId" } },
+            { $project: { likesData: 0 } },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "_id",
+                    foreignField: "postId",
+                    as: "commentsData"
+                }
+            },
+            { $addFields: { comments: "$commentsData.comment" } },
+            { $project: { commentsData: 0 } },
+            {
+                $project: {
+                    _id: 0,
+                    postId: "$_id",
+                    userId: "$user",
+                    likes: 1,
+                    comments: 1
+                }
+            }
+        ])
+        res.json({
+            status: 200,
+            message: "Success",
+            data: reqDetails
+        })
+    } catch (error) {
+        console.log(error)
+        throw new errorHandler(500, "Internal Server Error")
+    }
 })
 
 app.use((req, res, next) => {
-    throw new errorHandler(404,"Page does not exist!")
+    throw new errorHandler(404, "Page does not exist!")
 });
 
 app.use(errorHandlerMiddleware)
 
-app.listen(3000,async ()=>{
+app.listen(3000, async () => {
     await connectToDb()
     console.log("Server is up!")
 })
